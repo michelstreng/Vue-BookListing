63fd9d8a6f66fb94592cf736dcf343821121013f
Michel Streng <m.streng@rdc.nl>
2020-08-12 17:37:50 +0200

    added books

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -3,6 +3,9 @@
         <h1>
             {{ title }}
         </h1>
+        <ul v-for="book in books" :key="title">
+            <li>{{book.title}}, {{book.author}}</li>
+        </ul>
     </div>
 </template>
 
@@ -11,7 +14,12 @@ export default {
     name: "BookList",
     data() {
         return {
-            title: "All Books"
+            title: "All Books",
+            books: [
+                {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+                {title: 'American Gods', author: 'Neil Gaiman'},
+                {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
+            ]
         }
     }
 }
@@ -20,5 +28,13 @@ export default {
 <style>
     h1, h2 { 
         font-weight: normal
+    }
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+    li {
+        display: block;
+        margin: 0 10px;
     }
 </style>
